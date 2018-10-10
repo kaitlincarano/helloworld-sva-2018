28434ccb4694ea15fac37f1631eba173c860586c
KCarano <43015577+kaitlincarano@users.noreply.github.com>
2018-10-10 16:55:34 -0400

    Update and rename SerialPortSensorRead to SerialPortSensorRead.js

--- SerialPortSensorRead.js
+++ SerialPortSensorRead.js
@@ -7,7 +7,7 @@ serial = new p5.SerialPort();
 serial.open("/dev/cu.usbserial-DN051BT1");
 serial.onData(gotData);
 }
-// Ok here comes daya!
+// Ok here comes data!
 function gotData() {
 // Step 4a: read the data
 var currentString = serial.readLine(); // read the incoming string
@@ -16,9 +16,14 @@ latestData = Number(currentString); // save it for the draw method
 }
 function draw() {
 background(255, 255, 255);
-fill(0, 191, 196);
-// Step 4b: use the data
 var data = map(latestData, 0, 1023, 0, height);
+fill(0, data, 196);
+// Step 4b: use the data
 ellipse(250, 250, data);
 text(data, 10, 10);
+text("happy wednesday", 25, data);
+
+var location = map(latestData, 0,1023, 0, width);
+text("what's up", location, data);
+
 }
